2e6371ce07656ef0b3703a07ce7c4081cc24fb5c
Eddie A Linares <eddie.a.linares@gmail.com>
2019-05-14 18:40:33 -0500

    Check Spotify Call

--- liri.js
+++ liri.js
@@ -1,22 +1,29 @@
 require("dotenv").config();
-var Spotify = require('node-spotify-api');
 var axios = require("axios");
-var bandsintown = require('bandsintown')("codingbootcamp");
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 var action = process.argv[2];
 var request = process.argv[3];
 
-if (action === "spotify-this-song"){
-    spotify.search({ type: 'track', query: request }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-      
-      console.log(data); 
-      });
-} if (action === "concert-this"){
-    bandsintown.getArtistEventList(process.argv[3]).then(function(events) {
-      console.log(events);
-    });
+if (action === "spotify-this-song") {
+  spotifyCall();
+} if (action === "concert-this") {
+  bandsintownCall();
+}
+
+function spotifyCall() {
+  var Spotify = require('node-spotify-api');
+  var spotify = new Spotify(keys.spotify);
+  spotify.search({ type: 'track', query: request , limit:1 }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    console.log(JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
+  })
+}
+
+function bandsintownCall() {
+  var bandsintown = require('bandsintown')("codingbootcamp");
+  bandsintown.getArtistEventList(process.argv[3]).then(function (events) {
+    console.log(events);
+  });
 }
